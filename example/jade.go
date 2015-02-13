b4324a1af3209f90414d2cc4daea36a16ff764f6
Joker <nice.way@live.com>
2015-02-13 15:54:14 +0300

    add example

---
+++ example/jade.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"github.com/Joker/jade"
+)
+
+
+func main() {
+	dat, err := ioutil.ReadFile("template.jade")
+	if err != nil {
+		fmt.Printf("ReadFile error: %v", err)
+		return
+	}
+
+	tmpl, err := jade.New("jade_tpl").Parse(string(dat), "", "", make( map[string]*jade.Tree ), nil)
+	if err != nil {
+		fmt.Printf("Parse error: %v", err)
+		return
+	}
+
+	fmt.Printf( "\n\nOutput:\n\n"  )
+	fmt.Printf( tmpl.Root.String() )
+}
