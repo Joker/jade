d6baf4385d644ccb0037bf2e97d5eefd20e74d22
Joker <nice.way@live.com>
2018-04-18 14:12:18 +0300

    add lexer test

--- jade_test.go
+++ jade_test.go
@@ -13,12 +13,11 @@ import (
 	"github.com/Joker/hpp"
 )
 
-func TestJadeExamples(t *testing.T) {
-	PrettyOutput = false
+func examination(test func(dat string) (string, error), ext, path string, t *testing.T) {
 
-	files, _ := ioutil.ReadDir("./testdata")
+	files, _ := ioutil.ReadDir(path)
+
 	var name, fext string
-
 	for _, file := range files {
 		name = file.Name()
 		fext = filepath.Ext(name)
@@ -27,30 +26,35 @@ func TestJadeExamples(t *testing.T) {
 			continue
 		}
 
-		fmt.Println("_________" + name)
-
-		dat, err := ioutil.ReadFile("testdata/" + name)
+		dat, err := ioutil.ReadFile(path + name)
 		if err != nil {
+			fmt.Println("_________" + name)
 			fmt.Printf("--- FAIL: ReadFile error: %v\n\n", err)
 			t.Fail()
 			continue
 		}
 
-		tpl, err := Parse(name, string(dat))
+		tpl, err := test(string(dat))
 		if err != nil {
-			fmt.Printf("--- FAIL: Parse error: %v\n\n", err)
+			fmt.Println("_________" + name)
+			fmt.Printf("--- FAIL: test run() error: \n%s\n\n", err)
 			t.Fail()
 			continue
 		}
-		tmpl := bufio.NewScanner(bytes.NewReader(hpp.Print(strings.NewReader(tpl))))
+
+		tmpl := bufio.NewScanner(strings.NewReader(tpl))
 		tmpl.Split(bufio.ScanLines)
 
-		inFile, err := os.Open("testdata/" + strings.TrimSuffix(name, fext) + ".html")
+		inFile, err := os.Open(path + strings.TrimSuffix(name, fext) + ext)
 		if err != nil {
+
+			// make files
+			ioutil.WriteFile(path+strings.TrimSuffix(name, fext)+ext, []byte(tpl), 0644)
+
 			fmt.Println("```", tpl, "\n\n```")
-			ioutil.WriteFile("testdata/"+strings.TrimSuffix(name, fext)+".html", hpp.Print(strings.NewReader(tpl)), 0644)
 			continue
 		}
+
 		html := bufio.NewScanner(inFile)
 		html.Split(bufio.ScanLines)
 
@@ -64,6 +68,9 @@ func TestJadeExamples(t *testing.T) {
 			line += 1
 
 			if strings.Compare(a, b) != 0 && nilerr < 4 {
+				if nilerr == 0 {
+					fmt.Println("_________" + name)
+				}
 				fmt.Printf("%s\n%s\n%d^___________________________\n", a, b, line)
 				nilerr += 1
 				t.Fail()
@@ -71,10 +78,55 @@ func TestJadeExamples(t *testing.T) {
 		}
 		inFile.Close()
 
-		if nilerr == 0 {
-			fmt.Println("    PASS\n ")
-		} else {
-			fmt.Println("--- FAIL\n ")
+		if nilerr != 0 {
+			fmt.Print("--- FAIL\n\n")
+		}
+		// } else { fmt.Print("    PASS\n\n") }
+	}
+}
+
+func lexerTest(dat string) (string, error) {
+	var buf bytes.Buffer
+
+	l := lex("test", dat)
+	for i := range l.items {
+		switch {
+		case i.typ == itemError:
+			buf.WriteString("\n\n\nError:\n\t")
+			buf.WriteString(fmt.Sprintf("%s  line: %d", i.val, i.line))
+			buf.WriteString("\n\n\n")
+			return "", fmt.Errorf("%s", buf.String())
+		case i.typ == itemEOF:
+			buf.WriteString("\nEOF")
+		case i.typ == itemEndL:
+			buf.WriteByte('\n')
+		case i.typ == itemEmptyLine:
+			buf.WriteString(i.val)
+		case i.typ == itemIdent:
+			buf.WriteString(i.val)
+		default:
+			buf.WriteString(fmt.Sprintf("[%d  %s   \"%s\"]\t", i.depth, i.typ, i.val))
 		}
 	}
+	return buf.String(), nil
+}
+
+func xTestJadeLex(t *testing.T) {
+	examination(lexerTest, ".lex", "./testdata/", t)
+}
+
+//
+
+func parserTest(text string) (string, error) {
+	outTpl, err := New("test").Parse(text)
+	if err != nil {
+		return "", err
+	}
+	b := new(bytes.Buffer)
+	outTpl.WriteIn(b)
+	return string(hpp.Print(b)), nil
+}
+
+func TestJadeParse(t *testing.T) {
+	examination(parserTest, ".html", "./testdata/", t)
 }
